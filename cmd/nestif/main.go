a85bf22a1aa0b5c5ec47b997af1d5d6a45dc871a
nakabonne <nakabonne@gmail.com>
2020-01-13 12:18:40 +0900

    Change license from MIT to BSD 2-Clause

--- cmd/nestif/main.go
+++ cmd/nestif/main.go
@@ -1,3 +1,8 @@
+// Copyright 2020 Ryo Nakao <nakabonne@gmail.com>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
